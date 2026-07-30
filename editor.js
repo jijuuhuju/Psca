e635e8393dc33bc6a4db0e62d40c638e21209079
jijuuhuju <ryota.summer7@icloud.com>
2026-07-30 19:46:46 +0900

    Update editor.js

--- editor.js
+++ editor.js
@@ -1,25 +1,36 @@
-
 function savePSCA(){
 
     const code = document.getElementById("code").value;
+
+    let name = document.getElementById("fileName").value;
+
+    if(name === ""){
+        name = "main";
+    }
+
+    if(!name.endsWith(".psca")){
+        name += ".psca";
+    }
+
 
     const file = new Blob(
         [code],
         { type: "application/octet-stream" }
     );
 
+
     const url = URL.createObjectURL(file);
 
     const link = document.createElement("a");
 
     link.href = url;
-    link.download = "program.psca";
+    link.download = name;
+
 
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
 
-    setTimeout(() => {
-        URL.revokeObjectURL(url);
-    }, 1000);
+
+    URL.revokeObjectURL(url);
 }
